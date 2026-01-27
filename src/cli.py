9d09492450bfb560536cfd206dec9e44404ab305
Marcos Gabriel <marcosdevse@gmail.com>
2026-01-26 21:18:16 -0400

    fix: Syntax Error Fixing

--- src/cli.py
+++ src/cli.py
@@ -13,7 +13,7 @@ def _version_callback(value: bool) -> None:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
 
-@app.callback
+@app.callback()
 def main(
     version: Optional[bool] = typer.Option(
         None,
