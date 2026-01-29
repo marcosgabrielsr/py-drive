c10fefaedc8ac35ddb55a718c1b651f8b490b9b9
Marcos Gabriel <marcosdevse@gmail.com>
2026-01-29 19:39:11 -0400

    feat: Add not_trashed parameter

--- src/core/drive.py
+++ src/core/drive.py
@@ -5,7 +5,13 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-def build_query(only_folders:bool=False,only_my_own:bool=True,name:str=None,in_name:str=None) -> str:
+def build_query(
+        only_folders:bool=False,
+        only_my_own:bool=True,
+        name:str=None,
+        in_name:str=None,
+        not_trashed:bool=False
+    ) -> str:
     """Build the query to filter the search on google drive
 
     Args:
@@ -13,12 +19,12 @@ def build_query(only_folders:bool=False,only_my_own:bool=True,name:str=None,in_n
         only_not_shared: bool, option to only retrieve files where I am one of the owners.
         name: str, Name of the file to be searched for.
         in_name: str, text that can be part of the file name.
+        not_trashed: str, option to pick up only files that not in trash.
     
     Returns:
         Return a string that is the query that will be used on the Google Drive API v3 request.
     """
-    query = "trashed=false"
-
+    query = ("trashed=false","trashed=true")[not_trashed]
     query = (query,f"{query} and mimeType='application/vnd.google-apps.folder'")[only_folders]
     query = (query,f"{query} and 'me' in owners")[only_my_own]
     query = (query,f"{query} and name = '{name}'")[name is not None]
@@ -33,11 +39,12 @@ def list_files(
         folders:bool=False,
         my_own:bool=True,
         name:str=None,
-        in_name:str=None
+        in_name:str=None,
+        not_trashed:bool=True
     ) -> list:
     """List and search files from drive
     """
-    query = build_query(only_folders=folders,only_my_own=my_own,name=name,in_name=in_name)
+    query = build_query(only_folders=folders,only_my_own=my_own,name=name,in_name=in_name,not_trashed=not_trashed)
     files = []
     page_token = None
 
