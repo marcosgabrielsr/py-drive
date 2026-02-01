9b85f913db7e8502ca92d75236bd4f0cd2119073
Marcos Gabriel <marcosdevse@gmail.com>
2026-02-01 20:58:08 -0300

    feat: Add DEFAULT_DOWNLOAD_PATH

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEFAULT_DOWNLOAD_DIR = Path.home()/'Downloads'
 
 CREDENTIALS_FILE = BASE_DIR/os.getenv("GDRIVE_CREDENTIALS", "credentials.json")
 TOKEN_FILE = BASE_DIR/os.getenv("GDRIVE_TOKEN", "token.json")
